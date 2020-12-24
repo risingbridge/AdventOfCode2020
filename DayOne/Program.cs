5922c52aa7962933c5b1b78004c5e4f6c626f29c
Håvard Risebrobakken <haavard@risebro.net>
2020-12-24 16:11:36 +0100

    Add project files.

---
+++ DayOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DayOne
+{
+	class Program
+	{
+		public static string InputFile = "./input.txt";
+		static void Main(string[] args)
+		{
+			List<int> inputs = new List<int>();
+			foreach (string line in File.ReadLines(InputFile))
+			{
+				inputs.Add(int.Parse(line));
+			}
+			foreach (int x in inputs)
+			{
+				foreach (int y in inputs)
+				{
+					foreach (int z in inputs)
+					{
+						if(x+y+z == 2020)
+						{
+							Console.WriteLine(x * y * z);
+						}
+					}
+				}
+			}
+		}
+	}
+}
