12a3a12d8640bc3fdaaed9d7fb6338fd89b48d5c
Håvard Risebrobakken <haavard@risebro.net>
2020-12-27 23:09:24 +0100

    Day eight complete, day 7 not complete yet.

--- DayEight/Program.cs
+++ DayEight/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DayEight
 {
@@ -6,7 +8,102 @@ namespace DayEight
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Instructions:
+			//ACC - Legger til eller trekker fra accumulatoren
+			//JMP - Hopper til ny kodelinje, relativ til denne, f.eks jmp +4 hopper fire linjer videre
+			//NOP - No operations. Går videre til neste.
+			//
+
+			string[] inputArray = File.ReadAllLines("./input.txt");
+			List<Operation> ProgramQueue = new List<Operation>();
+			foreach (string item in inputArray)
+			{
+				string[] i = item.Split(" ");
+				Operation o = new Operation
+				{
+					Instruction = i[0],
+					Value = int.Parse(i[1])
+				};
+				ProgramQueue.Add(o);
+			}
+
+			RunProgram(ProgramQueue);
 		}
+
+		static void RunProgram(List<Operation> PQ)
+		{
+			int programCounter = 0;
+			int accumulator = 0;
+			bool halt = false;
+			bool loopDetected = false;
+			List<int> testedPositions = new List<int>();
+
+			List<int> previouslyVisited = new List<int>();
+			while (!halt)
+			{
+				previouslyVisited.Add(programCounter);
+				//Console.Clear();
+				if(programCounter >= PQ.Count)
+				{
+					halt = true;
+					break;
+				}
+				Operation currentOP = PQ[programCounter];
+				//Console.WriteLine($"Accumulator: {accumulator}, Program counter: {programCounter}, Inst: {currentOP.Instruction}, val: {currentOP.Value}");
+				string inst = currentOP.Instruction;
+
+				if (loopDetected)
+				{
+					if (inst == "jmp" && !testedPositions.Contains(programCounter) || inst == "nop" && !testedPositions.Contains(programCounter))
+					{
+						loopDetected = false;
+						Console.WriteLine($"Loop detected, trying new fix at line {programCounter}");
+						testedPositions.Add(programCounter);
+						if (inst == "jmp")
+						{
+							inst = "nop";
+						}
+						else if (inst == "nop")
+						{
+							inst = "jmp";
+						}
+					}
+				}
+
+				if(inst == "jmp")
+				{
+					programCounter += currentOP.Value;
+				}else if(inst == "acc")
+				{
+					accumulator += currentOP.Value;
+					programCounter++;
+				}else if(inst == "nop")
+				{
+					programCounter += 1;
+				}
+
+				//Loop detected!
+				if(previouslyVisited.Contains(programCounter))
+				{
+					previouslyVisited.Clear();
+					accumulator = 0;
+					programCounter = 0;
+					loopDetected = true;
+				}
+				if(programCounter > PQ.Count)
+				{
+					halt = true;
+				}
+				//Console.ReadKey();
+			}
+			Console.WriteLine($"Program halted. Accumulator: {accumulator}");
+		}
+	}
+
+	class Operation
+	{
+		public string Instruction { get; set; }
+		public int Value { get; set; }
+
 	}
 }
---
+++ DaySeven/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DaySeven
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Parse input
+			string[] rules = File.ReadAllLines("./input.txt");
+			Dictionary<string, List<string>> bagRules = new Dictionary<string, List<string>>();
+			foreach (string rule in rules)
+			{
+				string[] bagRule = rule.Split(" contain ");
+				List<string> childs = bagRule[1].Split(",").Select(x => x.Replace("bags.", "").Replace("bag.", "").Replace("bags", "").Replace("bag", "").Trim()).ToList();
+
+				if (!bagRules.ContainsKey(bagRule[0]))
+				{
+					bagRules.Add(bagRule[0].Replace("bags ", ""), childs);
+				}
+			}
+
+			PartOne(bagRules);
+		}
+
+		private static void PartOne(Dictionary<string, List<string>> Bags)
+		{
+			List<string> canContainGold = new List<string>();
+			int canContainGoldCount = 0;
+			foreach (KeyValuePair<string, List<string>> item in Bags)
+			{
+				foreach (string b in item.Value)
+				{
+					if(string.Concat(b.Where(char.IsLetter)) == "shinygold")
+					{
+						canContainGold.Add(item.Key);
+						canContainGoldCount++;
+					}
+				}
+			}
+
+			while(canContainGold.Count > 0)
+			{
+				string currentCheck = canContainGold[0];
+				canContainGold.RemoveAt(0);
+				foreach (KeyValuePair<string, List<string>> item in Bags)
+				{
+					foreach (string b in item.Value)
+					{
+						Console.WriteLine($"Checking if {b} == {currentCheck}");
+						if(string.Concat(b.Where(char.IsLetter)) == string.Concat(currentCheck.Where(char.IsLetter)).Replace("bags",""))
+						{
+							canContainGold.Add(item.Key);
+							canContainGoldCount++;
+						}
+					}
+				}
+			}
+			Console.WriteLine($"{canContainGoldCount} bags can hold the gold bag");
+		}
+	}
+}
