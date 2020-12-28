2ec14fdfb40bbfe9477419c7cc65b2e8244bf0f9
Håvard Risebrobakken <haavard@risebro.net>
2020-12-28 20:33:26 +0100

    Day seven part one completed, working on part two

---
+++ DayNine/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DayNine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> inputList = new List<double>();
+			foreach (string item in File.ReadAllLines("./debug.txt"))
+			{
+				inputList.Add(double.Parse(item));
+			}
+			//int[] inputArray = File.ReadAllLines("./input.txt");
+
+
+			List<double> preambleList = new List<double>();
+			List<double> preambleSums = new List<double>();
+			int preamble = 5;
+			bool skippedOnce = false;
+
+			for (int i = 0; i < preamble; i++)
+			{
+				preambleList.Add(inputList[i]);
+			}
+			preambleSums = new List<double>(CalculatePreambleSums(preamble, preambleList));
+
+			for (int i = preamble; i < inputList.Count; i++)
+			{
+				if (!preambleSums.Contains(inputList[i]))
+				{
+					if (skippedOnce)
+					{
+						Console.WriteLine($"{inputList[i]} is wrong!");
+					}
+					else
+					{
+						Console.WriteLine($"This does not compute, {inputList[i]}. Old sums is {preambleSums.Count} long");
+						preambleList.Add(inputList[i]);
+						preambleSums.Clear();
+						preambleSums = new List<double>(CalculatePreambleSums(preamble, preambleList));
+						Console.WriteLine($"Realculated, number of sums is {preambleSums.Count}");
+						skippedOnce = true;
+					}
+				}
+			}
+		}
+
+		static List<double> CalculatePreambleSums(int preamble, List<double> pl)
+		{
+			List<double> sums = new List<double>();
+
+			for (int x = 0; x < preamble; x++)
+			{
+				for (int y = 0; y < preamble; y++)
+				{
+					if(!sums.Contains(pl[x] + pl[y]))
+					{
+						sums.Add(pl[x] + pl[y]);
+					}
+				}
+			}
+
+			return sums;
+		}
+	}
+}
--- DaySeven/Program.cs
+++ DaySeven/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace DaySeven
 {
@@ -9,57 +10,120 @@ namespace DaySeven
 	{
 		static void Main(string[] args)
 		{
+			string inputFile = "./input.txt";
+			string[] inputArray = File.ReadAllLines(inputFile);
+			List<Bag> bags = new List<Bag>();
 			//Parse input
-			string[] rules = File.ReadAllLines("./input.txt");
-			Dictionary<string, List<string>> bagRules = new Dictionary<string, List<string>>();
-			foreach (string rule in rules)
+			foreach (string item in inputArray)
 			{
-				string[] bagRule = rule.Split(" contain ");
-				List<string> childs = bagRule[1].Split(",").Select(x => x.Replace("bags.", "").Replace("bag.", "").Replace("bags", "").Replace("bag", "").Trim()).ToList();
-
-				if (!bagRules.ContainsKey(bagRule[0]))
+				string itemFix = item.Replace("bags", "").Replace("bag", "").Replace(".", "");
+				string[] itemSplit = itemFix.Split(" contain ");
+				string[] childs = itemSplit[1].Split(",");
+				foreach (string child in childs)
 				{
-					bagRules.Add(bagRule[0].Replace("bags ", ""), childs);
+					Bag bag = new Bag
+					{
+						Name = Regex.Replace(child, "[0-9]", "").Trim(),
+						Parent = Regex.Replace(itemSplit[0], "[0-9]", "").Trim()
+					};
+					bags.Add(bag);
 				}
 			}
 
-			PartOne(bagRules);
+			SolvePartOne(bags);
+			SolvePartTwo(inputFile);
 		}
 
-		private static void PartOne(Dictionary<string, List<string>> Bags)
+		static void SolvePartOne(List<Bag> bList)
 		{
 			List<string> canContainGold = new List<string>();
-			int canContainGoldCount = 0;
-			foreach (KeyValuePair<string, List<string>> item in Bags)
+			List<string> haveChecked = new List<string>();
+			List<string> haveBag = new List<string>();
+			int bagCount = 0;
+			foreach (Bag bag in bList.Where(b => b.Name == "shiny gold"))
 			{
-				foreach (string b in item.Value)
-				{
-					if(string.Concat(b.Where(char.IsLetter)) == "shinygold")
-					{
-						canContainGold.Add(item.Key);
-						canContainGoldCount++;
-					}
-				}
+				canContainGold.Add(bag.Parent);
+				//Console.WriteLine($"{bag.Parent} can contain the gold bag");
+				haveBag.Add(bag.Parent);
+				bagCount++;
 			}
-
-			while(canContainGold.Count > 0)
+			while (canContainGold.Count > 0)
 			{
-				string currentCheck = canContainGold[0];
+				string bagToCheck = canContainGold[0];
+				haveChecked.Add(bagToCheck);
 				canContainGold.RemoveAt(0);
-				foreach (KeyValuePair<string, List<string>> item in Bags)
+				foreach (Bag bag in bList.Where(b => b.Name == bagToCheck))
 				{
-					foreach (string b in item.Value)
+					if(bag.Parent != "no other" || bag.Parent != null)
 					{
-						Console.WriteLine($"Checking if {b} == {currentCheck}");
-						if(string.Concat(b.Where(char.IsLetter)) == string.Concat(currentCheck.Where(char.IsLetter)).Replace("bags",""))
+						if (!haveChecked.Contains(bag.Parent) && !haveBag.Contains(bag.Parent))
 						{
-							canContainGold.Add(item.Key);
-							canContainGoldCount++;
+							bagCount++;
+							canContainGold.Add(bag.Parent);
+							//Console.WriteLine($"{bag.Parent} can contain gold");
+							haveBag.Add(bag.Parent);
 						}
 					}
 				}
 			}
-			Console.WriteLine($"{canContainGoldCount} bags can hold the gold bag");
+			Console.WriteLine($"Part One Solution: {bagCount}");
 		}
+
+		static void SolvePartTwo(string inputFile)
+		{
+			string[] inputArray = File.ReadAllLines(inputFile);
+			List<BagTwo> bags = new List<BagTwo>();
+			foreach (string item in inputArray)
+			{
+				string parent = item.Split(" contain ")[0];
+				string childString = item.Split(" contain ")[1];
+				List<BagChild> childs = new List<BagChild>();
+				string[] childArray = childString.Split(",");
+				foreach (string c in childArray)
+				{
+					int count = c[0];
+					string name = Regex.Replace(c, "[0-9]", "").Trim();
+					BagChild child = new BagChild
+					{
+						Name = name.Replace("bags", "").Replace("bag", "").Replace(".", ""),
+						count = count
+					};
+					childs.Add(child);
+				}
+				BagTwo bag = new BagTwo
+				{
+					Name = Regex.Replace(parent, "[0-9]", "").Replace("bags", "").Replace("bag", "").Replace(".", "").Trim(),
+					Childs = childs
+				};
+				bags.Add(bag);
+			}
+
+			foreach (BagTwo bag in bags.Where(b => b.Name == "shiny gold"))
+			{
+				Console.WriteLine($"Shiny gold bag contains:");
+				foreach (BagChild child in bag.Childs)
+				{
+					Console.WriteLine($"{child.Name}");
+				}
+			}
+		}
+	}
+
+	class Bag
+	{
+		public string Name { get; set; }
+		public string Parent { get; set; }
+	}
+
+	class BagTwo
+	{
+		public string Name { get; set; }
+		public List<BagChild> Childs { get; set; }
+	}
+
+	class BagChild
+	{
+		public string Name { get; set; }
+		public int count { get; set; }
 	}
 }
